e94da63e4fc9aa3a7e7922b7fbc4b4ba68b57ad0
tiagoamp <tiagoamp@gmail.com>
2023-03-01 21:25:05 -0300

    Adiciona lib de icones

--- src/components/Cabecalho/index.js
+++ src/components/Cabecalho/index.js
@@ -1,4 +1,7 @@
 import { Link } from 'react-router-dom';
+import { BsCoin }  from 'react-icons/bs';
+import { HiUsers } from 'react-icons/hi';
+import { TbReportAnalytics } from 'react-icons/tb';
 import './styles.css';
 
 export default function Cabecalho() {
@@ -8,9 +11,9 @@ export default function Cabecalho() {
             Coin Market
         </span>
         <nav>
-            <Link to='/'> Cotações </Link>
-            <Link to='usuarios'> Usuários </Link>
-            <Link to='relatorios'> Relatórios </Link>
+            <Link to='/'> <BsCoin/> Cotações </Link>
+            <Link to='usuarios'> <HiUsers/> Usuários </Link>
+            <Link to='relatorios'> <TbReportAnalytics/> Relatórios </Link>
         </nav>
     </header>
   )
