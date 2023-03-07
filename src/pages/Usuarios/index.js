f7d088ce118976cb130eb8dd74b023a6f0ac6513
tiagoamp <tiagoamp@gmail.com>
2023-03-06 21:46:49 -0300

    Implementa consumo de dados de uma API com useEffect

--- src/pages/Usuarios/index.js
+++ src/pages/Usuarios/index.js
@@ -1,7 +1,32 @@
+import { useEffect, useState } from 'react';
 import './styles.css';
 
 export default function Usuarios() {
+
+  const [usuarios, setUsuarios] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users?_limit=5')
+      .then(resp => resp.json())
+      .then(dados => setUsuarios(dados))
+      .catch(erro => console.log(erro));
+  }, [])
+  
+
   return (
-    <h1 className='titulo-usuarios'>componente de pagina de usuarios</h1>
+    <>
+      <h1 className='titulo-usuarios'></h1>
+      {
+        usuarios.map(u => {
+          return (
+            <div key={u.id} className="container-usuarios">
+              <span>{u.id}</span> - 
+              <span>{u.name}</span> - 
+              <span>{u.email}</span>
+            </div>
+          )
+        })
+      }
+    </>
   )
 }
