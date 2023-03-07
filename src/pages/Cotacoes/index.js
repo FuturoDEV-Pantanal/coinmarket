f709862157abc65bab3e4e04c0ec06de818c2768
tiagoamp <tiagoamp@gmail.com>
2023-03-06 21:26:20 -0300

    Implementa recuperacao de dados de criptos do arquivo json

--- src/pages/Cotacoes/index.js
+++ src/pages/Cotacoes/index.js
@@ -1,14 +1,20 @@
+import { useEffect, useState } from 'react';
 import Coin from '../../components/Coin';
 import './styles.css';
 
-const criptos = [
-    { id: 1, sigla: 'BTC', nome: 'BitCoin', preco: 115000.25, variacaoPerc: 3.55 }, 
-    { id: 2, sigla: 'ETH', nome: 'Ethereum', preco: 8114.03, variacaoPerc: 1.94 },
-    { id: 3, sigla: 'USDC', nome: 'USDCoin', preco: 5.09, variacaoPerc: -0.15 },
-    { id: 4, sigla: 'LTC', nome: 'LiteCoin', preco: 447.33, variacaoPerc: 1.99 } 
-];
-
 export default function Cotacoes() {
+
+    const [criptos, setCriptos] = useState([]);
+
+    useEffect(() => {
+      fetch('dados.json')
+        .then(resp => resp.json())
+        .then(dados => setCriptos(dados))
+        .catch(erro => console.log(erro));
+
+    }, [])
+    
+
   return (
     <table className="tabela-cotacoes">
         <thead>
